6c618f9d6a32ba18d60177781b3b45a99f964394
maalejmaalej <56475552+maalejmaalej@users.noreply.github.com>
2021-09-06 02:30:07 +0100

    fix bugs

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,3 +1,5 @@
-export const apiURL= process.env.NODE_ENV === "production" ?"https://chatpiximind.herokuapp.com":"http://localhost:8000";
+export const apiURL= process.env.NODE_ENV === 'development'
+? "http://localhost:8000"
+: process.env.REACT_APP_URI_CONVERTER;
 
 
